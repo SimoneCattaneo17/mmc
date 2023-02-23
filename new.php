4135b4f78a8e2bfb973e954b341dd8096437d734
SimoneCattaneo16 <cattaneo.simone.studente@itispaleocapa.it>
2023-02-23 23:08:30 +0100

    saves DVRs data in a table needs to create a DVR document with the data saved in the table

--- new.php
+++ new.php
@@ -9,44 +9,10 @@
 <body>
     <?php
     require __DIR__ . '/functions.php';
-    session_start();
 
     if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
         $name = $_SESSION['username'];
         $password = $_SESSION['password'];
-        /*
-        echo '
-            <section class="vh-100" style="background-color: #508bfc;">
-                <div class="container py-5 h-100">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                                <div class="card-body p-5 text-center">
-
-                                    <h3 class="mb-5">Creazione DVR Aziendale</h3>
-
-                                    <form method="POST" action="homepage.php">
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="typeEmailX-2">Ragione sociale</label>
-                                            <input type="text" name="username" id="typeUser-2" class="form-control form-control-lg" />
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="typePasswordX-2">Password</label>
-                                            <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                                        </div>
-
-                                        <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-                                    </form>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        ';
-        */
         echo '
         <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
